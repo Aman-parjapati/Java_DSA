14d8b59aa07ed7622d2f8363007700c1d65d4335
Aman-parjapati <a63092253@gmail.com>
2025-04-04 01:32:45 +0530

    git commit -m "1st commit"

---
+++ src/BitManipulation/TurnOffRightMostBit.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class TurnOffRightMostBit {
+    public static void main(String[] args) {
+//        int this k is not given so we need to find k
+//        method 1 find the k by creating a copy of the number and then use while loop with condt n%2==0
+//        if false means number is 1 and break the loop
+//        now we have k simply use bitwise and it will turn off the right most bit
+//        tc -> log base 2 n  or O(31)
+
+//        method 2
+//        return n & n-1
+//        this will turn off the bit
+//        tc -> O(1)
+    }
+}
