af3667cf6f202fa4ba18f0878416df2159c5acaf
Aman-parjapati <a63092253@gmail.com>
2025-04-10 20:35:15 +0530

    10-04-2025 bit manipulation

--- src/BitManipulation/powerof2.java
+++ src/BitManipulation/powerof2.java
@@ -5,6 +5,8 @@ public class powerof2 {
         System.out.println(1<<0);  // using left shift operator
         System.out.println(1<<1);
         System.out.println(1<<2); // tc = O(1)
-        System.out.println(1<<12);
+        System.out.println(1<<12);  // 1<<k is power of 2
+
+//          for a number be to power of 2 we can do (n & n-1) == 0  ->returns true or false
     }
 }
